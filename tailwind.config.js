a68bc4df3dac9d4d7f0976bb19e62b8e59518faf
tasfique86 <tasfiquerishad1217@gmail.com>
2025-10-27 00:02:04 +0600

    Set up the base file and folder structure for the rest of the app

---
+++ tailwind.config.js
@@ -0,0 +1,37 @@
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  // NOTE: Update this to include the paths to all files that contain Nativewind classes.
+  content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
+  presets: [require("nativewind/preset")],
+  theme: {
+    extend: {
+      colors: {
+        primary: "#FE8C00",
+        white: {
+              DEFAULT: "#ffffff",
+              100: "#fafafa",
+              200: "#FE8C00",
+        },
+        gray: {
+              100: "#878787",
+              200: "#878787",
+        },
+        dark: {
+              100: "#181C2E",
+        },
+        error: "#F14141",
+        success:"#2F9B65",
+      },
+      fontFamily: {
+  quicksand: ["QuickSand-Regular", "sans-serif"],
+  "quicksand-bold": ["QuickSand-Bold", "sans-serif"],
+  "quicksand-semibold": ["QuickSand-SemiBold", "sans-serif"],
+  "quicksand-light": ["QuickSand-Light", "sans-serif"],
+  "quicksand-medium": ["QuickSand-Medium", "sans-serif"],
+},
+
+    },
+  },
+  plugins: [],
+}
